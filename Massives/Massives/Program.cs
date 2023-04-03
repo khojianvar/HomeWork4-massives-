f1d7ef0a19aba942bc7589f7979dc3eb238193b6
khojianvar <khojianvarabdumukhtorov@gmail.com>
2023-04-03 21:23:03 +0500

    Array4

--- Massives/Massives/Program.cs
+++ Massives/Massives/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter n: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Enter k: ");
+            int k = int.Parse(Console.ReadLine());
+            haveKorNot(n, k);
+        }
+        public static void haveKorNot(int n, int k)
+        {
+            Random random = new Random();
+            int[] array1 = new int[n];
+            Console.WriteLine($"Before: ");
+            for (int i = 0; i < n; i++)
+            {
+                array1[i] = random.Next(1, 10);
+                Console.Write($"{array1[i]} ");
+            }
+            Console.WriteLine();
+            Console.WriteLine($"After: ");
+            for (int i = 0; i < n; i++)
+            {
+                if (array1[i] == k)
+                {
+                    Console.WriteLine("We have k in array :)");
+                    break;
+                }
+            }
+            Console.WriteLine("We have not K in array :(");
         }
     }
 }
